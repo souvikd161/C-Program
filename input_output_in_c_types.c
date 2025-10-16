c7aa23e6c128487aea21c0bc0050e823a78a1718
Souvik Das <25LBCS3333@culkomail.in>
2025-10-16 11:34:06 +0530

    Create input_output_in_c_types.c

---
+++ input_output_in_c_types.c
@@ -0,0 +1,11 @@
+//Enhance your coding skills, start writing your code here!!
+#include <stdio.h>
+
+int main() {
+    char input[3]; 
+    scanf("%s", input); 
+
+    printf("%c\n", input[0]); 
+    printf("%c\n", input[1]); 
+    return 0;
+}
